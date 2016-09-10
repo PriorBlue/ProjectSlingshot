388f9896a8b6823b5114bfc9d5689e272b7fae30
Marcus Ihde <marcusplate@googlemail.com>
2016-09-10 15:22:17 +0200

    Add love files.

---
+++ love2d/main.lua
@@ -0,0 +1,41 @@
+require("lib/postshader")
+
+function love.load()
+
+end
+
+function love.update(dt)
+
+end
+
+function love.draw()
+
+end
+
+function love.keypressed(key, scancode, isrepeat)
+
+end
+
+function love.keyreleased(key)
+
+end
+
+function love.textinput(text)
+
+end
+
+function love.mousepressed(x, y, button)
+
+end
+
+function love.mousereleased(x, y, button)
+
+end
+
+function love.mousemoved(x, y, dx, dy)
+
+end
+
+function love.wheelmoved(x, y)
+
+end
