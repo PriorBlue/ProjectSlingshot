fb9c7df9d04605014b212bbce1199e5539eda953
David Hubber <sergamer1980@gmail.com>
2016-09-10 20:05:51 +0200

    Ship path is calculated and rendered on screen.  Uses Euler integration (improve to Verlet next time)

--- love2d/lib/galaxy.lua
+++ love2d/lib/galaxy.lua
@@ -9,6 +9,10 @@ function CreateGalaxy(numStars, xcentre, ycentre, radius, seed)
   galaxy.Gconst = 20000.0
   galaxy.stars={}
 
+  galaxy.ship={}
+  galaxy.shippath = nil
+
+
   galaxy.Init = function(self)
     love.math.setRandomSeed(self.seed)
     for e=1,self.numStars do
@@ -31,7 +35,16 @@ function CreateGalaxy(numStars, xcentre, ycentre, radius, seed)
     table.insert(galaxy.stars, star)
   end
 
-  galaxy.CalculateForce = function(xship, yship)
+  galaxy.CreateShip = function(m,x,y,vx,vy)
+    local ship = galaxy.ship
+    ship.m = m
+    ship.x = x
+    ship.y = y
+    ship.vx = vx or 0.0
+    ship.vy = vy or 0.0
+  end
+
+  galaxy.CalculateAcceleration = function(xship, yship)
     local ax = 0.0
     local ay = 0.0
     for e=1,#galaxy.stars do
@@ -55,12 +68,62 @@ function CreateGalaxy(numStars, xcentre, ycentre, radius, seed)
     return ax,ay
   end
 
+  galaxy.CalculateShipPath = function(rangePath)
+    local ship = galaxy.ship
+    local ax,ay,rsqd
+    local x = galaxy.ship.x
+    local y = galaxy.ship.y
+    local vx = galaxy.ship.vx
+    local vy = galaxy.ship.vy
+
+    galaxy.shippath = {}
+    table.insert(galaxy.shippath, x)
+    table.insert(galaxy.shippath, y)
+
+    repeat
+      ax,ay = galaxy.CalculateAcceleration(x, y)
+      local asqd = ax*ax + ay*ay
+      local dt = 1.0*math.sqrt(galaxy.etaGrav / math.sqrt(asqd))
+      x = x + vx*dt
+      y = y + vy*dt
+      vx = vx + ax*dt
+      vy = vy + ay*dt
+      rsqd = (x - ship.x)*(x - ship.x) + (y - ship.y)*(y - ship.y)
+      table.insert(galaxy.shippath, x)
+      table.insert(galaxy.shippath, y)
+    until rsqd > rangePath*rangePath
+
+  end
+
+  galaxy.AdvanceShip = function(dt_full, ax_thrust, ay_thrust)
+    local dt_sum = 0.0
+    local ship = galaxy.ship
+    repeat
+      local ax,ay = galaxy.CalculateAcceleration(ship.x, ship.y)
+      ax = ax + ax_thrust
+      ay = ay + ay_thrust
+      local asqd = ax*ax + ay*ay
+      local dt = math.sqrt(galaxy.etaGrav / math.sqrt(asqd))
+      dt = math.min(dt, 1.0000001*(dt_full - dt_sum))
+      ship.x = ship.x + ship.vx*dt
+      ship.y = ship.y + ship.vy*dt
+      ship.vx = ship.vx + ax*dt
+      ship.vy = ship.vy + ay*dt
+      dt_sum = dt_sum + dt
+    until dt_sum >= dt_full
+  end
+
   galaxy.draw = function()
     love.graphics.setColor(255, 255, 255)
     for e=1,#galaxy.stars do
       local x = galaxy.stars[e].x
       local y = galaxy.stars[e].y
       love.graphics.circle("fill", x, y, 10.0*math.sqrt(galaxy.stars[e].m))
+    end
+    if galaxy.shippath then
+      DEBUG.path = #galaxy.shippath
+      love.graphics.setColor(0, 255, 0)
+      love.graphics.line(galaxy.shippath)
     end
   end
 
--- love2d/main.lua
+++ love2d/main.lua
@@ -2,52 +2,61 @@ require("lib/postshader")
 require("lib/galaxy")
 
 function love.load()
-	love.physics.setMeter(64) --the height of a meter our worlds will be 64px
-	world = love.physics.newWorld(0, 0, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
+	--love.physics.setMeter(64) --the height of a meter our worlds will be 64px
+	--world = love.physics.newWorld(0, 0, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
 
 	--let's create a ball
-	ship = {}
-	ship.body = love.physics.newBody(world, 650/2 - 100, 650/2, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
-	ship.shape = love.physics.newCircleShape(8) --the ball's shape has a radius of 20
-	ship.fixture = love.physics.newFixture(ship.body, ship.shape, 1) -- Attach fixture to body and give it a density of 1.
-	ship.fixture:setRestitution(0.9) --let the ball bounce
+	--ship = {}
+	--ship.body = love.physics.newBody(world, 0.0, 0.0, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
+	--ship.shape = love.physics.newCircleShape(8) --the ball's shape has a radius of 20
+	--ship.fixture = love.physics.newFixture(ship.body, ship.shape, 1) -- Attach fixture to body and give it a density of 1.
+	--ship.fixture:setRestitution(0.9) --let the ball bounce
 
-	milkyWay = CreateGalaxy(1000, 200.0, 200.0, 3000.0, 2)
+	milkyWay = CreateGalaxy(400, 0.0, 0.0, 3000.0, 2)
 	milkyWay:Init()
 	DEBUG = {}
+
+	milkyWay.CreateShip(1.0, 0.0, 0.0, 0.5, 0.0)
 
 end
 
 function love.update(dt)
-	world:update(dt) --this puts the world into motion
+	--world:update(dt) --this puts the world into motion
 
-  ax,ay = milkyWay.CalculateForce(ship.body:getX(), ship.body:getY())
+  --ax,ay = milkyWay.CalculateAcceleration(ship.body:getX(), ship.body:getY())
 
-	ship.body:applyForce(ax, ay)
-	print(ax,ay)
+  local ax = 0.0
+	local ay = 0.0
+
+
+	--ship.body:applyForce(ax, ay)
+	--print(ax,ay)
 
 	--here we are going to create some keyboard events
 	if love.keyboard.isDown("right") then --press the right arrow key to push the ball to the right
-		ship.body:applyForce(10, 0)
+		ax = ax + 10.0
 	elseif love.keyboard.isDown("left") then --press the left arrow key to push the ball to the left
-		ship.body:applyForce(-10, 0)
+		ax = ax - 10.0
 	elseif love.keyboard.isDown("up") then --press the up arrow key to set the ball in the air
-		ship.body:applyForce(0, -10)
+		ay = ay - 10
 	elseif love.keyboard.isDown("down") then --press the up arrow key to set the ball in the air
-		ship.body:applyForce(0, 10)
+		ay = ay + 10
 	end
+
+	milkyWay.AdvanceShip(dt, ax, ay)
+	milkyWay.CalculateShipPath(500.0)
 end
 
 function love.draw()
-	love.graphics.translate(-ship.body:getX() + love.graphics.getWidth() * 0.5, -ship.body:getY() + love.graphics.getHeight() * 0.5)
+	love.graphics.translate(-milkyWay.ship.x + love.graphics.getWidth() * 0.5, -milkyWay.ship.y + love.graphics.getHeight() * 0.5)
 
 	love.graphics.setColor(193, 47, 14) --set the drawing color to red for the ball
-	love.graphics.circle("fill", ship.body:getX(), ship.body:getY(), ship.shape:getRadius())
+	love.graphics.circle("fill", milkyWay.ship.x, milkyWay.ship.y, 8) --ship.body:getX(), ship.body:getY(), ship.shape:getRadius())
 
   milkyWay.draw()
 
 	love.graphics.origin()
-	local ax,ay = milkyWay.CalculateForce(ship.body:getX(), ship.body:getY())
+	local ax,ay = milkyWay.CalculateAcceleration(milkyWay.ship.x, milkyWay.ship.y)
 	local count = 0
 	for k,v in pairs(DEBUG) do
 		count = count + 1
