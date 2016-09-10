29173b96217aa1076316524639e9e4c27bf8ce18
David Hubber <sergamer1980@gmail.com>
2016-09-10 18:50:57 +0200

    Compute force from all galaxy stars correctly

--- love2d/lib/galaxy.lua
+++ love2d/lib/galaxy.lua
@@ -1,8 +1,12 @@
-function CreateGalaxy(numStars, radius, seed)
+function CreateGalaxy(numStars, xcentre, ycentre, radius, seed)
   local galaxy={}
   galaxy.numStars = numStars or 1
+  galaxy.xcentre = xcentre or 0.0
+  galaxy.ycentre = ycentre or 0.0
   galaxy.radius = radius or 1.0
   galaxy.seed = seed or 1
+  galaxy.etaGrav = 10.0
+  galaxy.Gconst = 20000.0
   galaxy.stars={}
 
   galaxy.Init = function(self)
@@ -15,7 +19,7 @@ function CreateGalaxy(numStars, radius, seed)
         y = radius*(1.0 - 2.0*love.math.random())
         rsqd = x*x + y*y
       until rsqd < radius*radius
-      self.CreateStar(m,x,y)
+      self.CreateStar(m, xcentre + x, ycentre + y)
     end
   end
 
@@ -27,12 +31,36 @@ function CreateGalaxy(numStars, radius, seed)
     table.insert(galaxy.stars, star)
   end
 
+  galaxy.CalculateForce = function(xship, yship)
+    local ax = 0.0
+    local ay = 0.0
+    for e=1,#galaxy.stars do
+      local m = galaxy.stars[e].m
+      local x = galaxy.stars[e].x
+      local y = galaxy.stars[e].y
+      local dx = x - xship
+      local dy = y - yship
+      local distsqd = dx*dx + dy*dy + galaxy.etaGrav*galaxy.etaGrav
+      local dist = math.sqrt(distsqd)
+      local invdist = 1.0/dist
+      local invdist3 = math.pow(invdist, 3.0)
+      ax = ax + galaxy.Gconst*m*dx*invdist3
+      ay = ay + galaxy.Gconst*m*dy*invdist3
+      DEBUG.dist = dist
+      DEBUG.ax = ax
+      DEBUG.ay = ay
+    end
+    DEBUG.ax = ax
+    DEBUG.ay = ay
+    return ax,ay
+  end
+
   galaxy.draw = function()
     love.graphics.setColor(255, 255, 255)
     for e=1,#galaxy.stars do
       local x = galaxy.stars[e].x
       local y = galaxy.stars[e].y
-      love.graphics.points(x,y)
+      love.graphics.circle("fill", x, y, 10.0*math.sqrt(galaxy.stars[e].m))
     end
   end
 
--- love2d/main.lua
+++ love2d/main.lua
@@ -5,72 +5,55 @@ function love.load()
 	love.physics.setMeter(64) --the height of a meter our worlds will be 64px
 	world = love.physics.newWorld(0, 0, true) --create a world for the bodies to exist in with horizontal gravity of 0 and vertical gravity of 9.81
 
-	objects = {} -- table to hold all our physical objects
-
 	--let's create a ball
-	objects.ball = {}
-	objects.ball.body = love.physics.newBody(world, 650/2 - 100, 650/2, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
-	objects.ball.shape = love.physics.newCircleShape(8) --the ball's shape has a radius of 20
-	objects.ball.fixture = love.physics.newFixture(objects.ball.body, objects.ball.shape, 1) -- Attach fixture to body and give it a density of 1.
-	objects.ball.fixture:setRestitution(0.9) --let the ball bounce
+	ship = {}
+	ship.body = love.physics.newBody(world, 650/2 - 100, 650/2, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
+	ship.shape = love.physics.newCircleShape(8) --the ball's shape has a radius of 20
+	ship.fixture = love.physics.newFixture(ship.body, ship.shape, 1) -- Attach fixture to body and give it a density of 1.
+	ship.fixture:setRestitution(0.9) --let the ball bounce
 
-	--let's create a ball 2
-	objects.ball2 = {}
-	objects.ball2.body = love.physics.newBody(world, 650/2 + 200, 650/2, "dynamic") --place the body in the center of the world and make it dynamic, so it can move around
-	objects.ball2.shape = love.physics.newCircleShape(128) --the ball's shape has a radius of 20
-	objects.ball2.fixture = love.physics.newFixture(objects.ball2.body, objects.ball2.shape, 1) -- Attach fixture to body and give it a density of 1.
-	objects.ball2.fixture:setRestitution(0.9) --let the ball bounce
-
-	milkyWay = CreateGalaxy(100, 300.0, 2)
+	milkyWay = CreateGalaxy(1000, 200.0, 200.0, 3000.0, 2)
 	milkyWay:Init()
+	DEBUG = {}
 
 end
 
 function love.update(dt)
 	world:update(dt) --this puts the world into motion
 
-	local dx = objects.ball2.body:getX() - objects.ball.body:getX()
-	local dy = objects.ball2.body:getY() - objects.ball.body:getY()
+  ax,ay = milkyWay.CalculateForce(ship.body:getX(), ship.body:getY())
 
-	local forceX = 0
-	local forceY = 0
-
-	if math.abs(dx) > 10 then
-		local dx2 = dx / (dx + dy)
-
-		forceX = math.min(50.0, 50.0 / (objects.ball2.body:getX() - objects.ball.body:getX())) * dx2
-	end
-
-	if math.abs(dy) > 10 then
-		local dy2 = dy / (dx + dy)
-
-		forceY = math.min(50.0, 50.0 / (objects.ball2.body:getY() - objects.ball.body:getY())) * dy2
-	end
-
-	objects.ball.body:applyForce(forceX, forceY)
+	ship.body:applyForce(ax, ay)
+	print(ax,ay)
 
 	--here we are going to create some keyboard events
 	if love.keyboard.isDown("right") then --press the right arrow key to push the ball to the right
-		objects.ball.body:applyForce(10, 0)
+		ship.body:applyForce(10, 0)
 	elseif love.keyboard.isDown("left") then --press the left arrow key to push the ball to the left
-		objects.ball.body:applyForce(-10, 0)
+		ship.body:applyForce(-10, 0)
 	elseif love.keyboard.isDown("up") then --press the up arrow key to set the ball in the air
-		objects.ball.body:applyForce(0, -10)
+		ship.body:applyForce(0, -10)
 	elseif love.keyboard.isDown("down") then --press the up arrow key to set the ball in the air
-		objects.ball.body:applyForce(0, 10)
+		ship.body:applyForce(0, 10)
 	end
 end
 
 function love.draw()
-	love.graphics.translate(-objects.ball.body:getX() + love.graphics.getWidth() * 0.5, -objects.ball.body:getY() + love.graphics.getHeight() * 0.5)
-	
-	love.graphics.setColor(193, 47, 14) --set the drawing color to red for the ball
-	love.graphics.circle("fill", objects.ball.body:getX(), objects.ball.body:getY(), objects.ball.shape:getRadius())
+	love.graphics.translate(-ship.body:getX() + love.graphics.getWidth() * 0.5, -ship.body:getY() + love.graphics.getHeight() * 0.5)
 
-	love.graphics.setColor(14, 47, 193) --set the drawing color to red for the ball
-	love.graphics.circle("fill", objects.ball2.body:getX(), objects.ball2.body:getY(), objects.ball2.shape:getRadius())
+	love.graphics.setColor(193, 47, 14) --set the drawing color to red for the ball
+	love.graphics.circle("fill", ship.body:getX(), ship.body:getY(), ship.shape:getRadius())
 
   milkyWay.draw()
+
+	love.graphics.origin()
+	local ax,ay = milkyWay.CalculateForce(ship.body:getX(), ship.body:getY())
+	local count = 0
+	for k,v in pairs(DEBUG) do
+		count = count + 1
+    love.graphics.print(k .. "   " .. v, 16, count*16)
+	end
+	--love.graphics.print(math.floor(ax) .. "                        " .. math.floor(ay), 16, 16)
 end
 
 function love.keypressed(key, scancode, isrepeat)
